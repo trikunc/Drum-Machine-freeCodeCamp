974df3fcd3298ca3314bdb1f8e185db36ce54cd0
Kunc <46520137+trikunc@users.noreply.github.com>
2020-11-22 00:49:42 +0700

    add sound

--- src/App.js
+++ src/App.js
@@ -1,29 +1,88 @@
-import React, { useState } from "react";
+import React, { createRef } from "react";
 import "./styles.css";
 
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
+
 const App = () => {
-  const [keys, setKeys] = useState([
-    "Q",
-    "W",
-    "E",
-    "A",
-    "S",
-    "D",
-    "Z",
-    "X",
-    "C"
-  ]);
   return (
     <div id="drum-machine" className="container">
       <div id="display" className="display">
-        {keys.map((key, index) => (
-          <Box text={key} key={index} />
+        {bankOne.map((key, index) => (
+          <Box text={key.keyTrigger} key={index} audioUrl={key.url} />
         ))}
       </div>
     </div>
   );
 };
 
-const Box = (props) => <div className="box">{props.text}</div>;
+const Box = ({ text, audioUrl }) => {
+  const sound = createRef();
+
+  const playSound = () => {
+    sound.current.play();
+  };
+
+  return (
+    <div className="box" onClick={playSound}>
+      {text}
+      <audio ref={sound} src={audioUrl} className="clip" id={text} />
+    </div>
+  );
+};
 
 export default App;
